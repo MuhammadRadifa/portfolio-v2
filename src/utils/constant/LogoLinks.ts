917c14eacd52656dc4a89300a25ad42e6904911b
MuhammadRadifa <m.difa123456@gmail.com>
2023-12-05 09:05:33 +0800

    🔥chore : add logo links

---
+++ src/utils/constant/LogoLinks.ts
@@ -0,0 +1,31 @@
+import { FaGithub, FaLinkedin } from 'react-icons/fa'
+import { RiTwitterXLine, RiInstagramLine } from 'react-icons/ri'
+import { MdMail } from 'react-icons/md'
+
+export const logoLinks = [
+  {
+    name: 'Github',
+    href: '',
+    icon: FaGithub,
+  },
+  {
+    name: 'Twitter',
+    href: '',
+    icon: RiTwitterXLine,
+  },
+  {
+    name: 'Instagram',
+    href: '',
+    icon: RiInstagramLine,
+  },
+  {
+    name: 'LikedIn',
+    href: '',
+    icon: FaLinkedin,
+  },
+  {
+    name: 'Mail',
+    href: '',
+    icon: MdMail,
+  },
+]
