dd3e3b7895f9cfb1ab53c500cf5b3221dd883c4d
MuhammadRadifa <m.difa123456@gmail.com>
2023-12-05 09:02:40 +0800

    🧼config: add tailwinds config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,6 +8,15 @@ const config: Config = {
   ],
   theme: {
     extend: {
+      colors: {
+        background: '#0F182B',
+        'primary-text': '#E2E8F0',
+        'secondary-text': '#A0AEC0',
+        'blue-sky': '#5FBDFF',
+      },
+      height: {
+        fullscreen: '100dvh',
+      },
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
         'gradient-conic':
