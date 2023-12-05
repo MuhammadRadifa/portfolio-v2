ba2710ba4cadb07f2610f1a54176d3db4c3af96a
MuhammadRadifa <m.difa123456@gmail.com>
2023-12-05 09:01:50 +0800

    🧼config: add image

--- next.config.js
+++ next.config.js
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'avatars.githubusercontent.com',
+        port: '',
+        pathname: '/muhammadradifa',
+      },
+    ],
+  },
+}
 
 module.exports = nextConfig
