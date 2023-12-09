0d7a99ac2e9095e03dbfb5c6a10a501212588c3c
MuhammadRadifa <m.difa123456@gmail.com>
2023-12-09 19:42:10 +0800

    🔥chore : add background

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,7 @@
 import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
 import './globals.css'
 import Navbar from '@/components/Navbar'
 import Footer from '@/components/Footer'
-
-const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
   title: 'Create Next App',
@@ -18,7 +15,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className="bg-background">
+      <body className="overflow-x-hidden">
         <Navbar />
         <main className="p-5 md:p-8">{children}</main>
         <Footer />
