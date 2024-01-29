dee5c9264e6933a5abf40b70339f5f8240740d04
MuhammadRadifa <m.difa123456@gmail.com>
2024-01-29 16:05:23 +0800

    🛠️refactor: refactor data constant

--- src/app/project/page.tsx
+++ src/app/project/page.tsx
@@ -1,49 +1,8 @@
 import ProjectCard from '@/components/common/ProjectCard'
 import TextSection from '@/components/common/TextSection'
-import foto1 from '@/assets/Project/foto1.png'
-import foto2 from '@/assets/Project/foto2.png'
-import foto3 from '@/assets/Project/foto3.png'
-import foto4 from '@/assets/Project/foto4.png'
-import foto5 from '@/assets/Project/foto5.png'
+import data from '@/utils/constant/Project'
 
 export default function Project() {
-  const data = [
-    {
-      title: 'Kalkulator Kesehatan',
-      image: foto1,
-      deskripsi: 'app for calculating health, such as BMI, BMR, and others.',
-      demo: 'https://kalkulator-kesehatan.netlify.app/',
-      repo: 'https://github.com/MuhammadRadifa/kalkulator-kesehatan',
-    },
-    {
-      title: 'Restful API - library',
-      image: foto2,
-      deskripsi:
-        'library management system API is an API used to manage library systems',
-      repo: 'https://github.com/MuhammadRadifa/API-Perpustakaan',
-    },
-    {
-      title: 'Restful API - Social Media',
-      image: foto3,
-      deskripsi:
-        'Social Media API is an API used to manage social media forum systems',
-      repo: 'https://github.com/MuhammadRadifa/api-comunity-app',
-    },
-    {
-      title: 'Learning Aja',
-      image: foto4,
-      deskripsi:
-        'Learning Aja is a website that provides learning materials for students.',
-      repo: 'https://github.com/MuhammadRadifa/amba-ajar',
-    },
-    {
-      title: 'Portfolio-V1',
-      image: foto5,
-      deskripsi:
-        'my first portfolio website. built with nextJS and TailwindCSS',
-      repo: 'https://github.com/MuhammadRadifa/portfolio',
-    },
-  ]
   return (
     <div className="">
       <TextSection text="it's My Projects." />
---
+++ src/utils/constant/Project.ts
@@ -0,0 +1,57 @@
+import foto1 from '@/assets/Project/foto1.png'
+import foto2 from '@/assets/Project/foto2.png'
+import foto3 from '@/assets/Project/foto3.png'
+import foto4 from '@/assets/Project/foto4.png'
+import foto5 from '@/assets/Project/foto5.png'
+import foto6 from '@/assets/Project/foto6.png'
+
+const data = [
+  {
+    title: 'Kalkulator Kesehatan',
+    image: foto1,
+    deskripsi: 'app for calculating health, such as BMI, BMR, and others.',
+    type: 'web',
+    demo: 'https://kalkulator-kesehatan.netlify.app/',
+    repo: 'https://github.com/MuhammadRadifa/kalkulator-kesehatan',
+  },
+  {
+    title: 'Restful API - library',
+    image: foto2,
+    deskripsi:
+      'library management system API is an API used to manage library systems',
+    type: 'web',
+    repo: 'https://github.com/MuhammadRadifa/API-Perpustakaan',
+  },
+  {
+    title: 'Restful API - Social Media',
+    image: foto3,
+    deskripsi:
+      'Social Media API is an API used to manage social media forum systems',
+    type: 'web',
+    repo: 'https://github.com/MuhammadRadifa/api-comunity-app',
+  },
+  {
+    title: 'Learning Aja',
+    image: foto4,
+    deskripsi:
+      'Learning Aja is a website that provides learning materials for students.',
+    type: 'web',
+    repo: 'https://github.com/MuhammadRadifa/amba-ajar',
+  },
+  {
+    title: 'Portfolio-V1',
+    image: foto5,
+    deskripsi: 'my first portfolio website. built with nextJS and TailwindCSS',
+    type: 'web',
+    repo: 'https://github.com/MuhammadRadifa/portfolio',
+  },
+  {
+    title: 'News App',
+    image: foto6,
+    deskripsi: 'my first android app. built with kotlin jetpack compose',
+    type: 'android',
+    repo: 'https://github.com/MuhammadRadifa/portfolio',
+  },
+]
+
+export default data
